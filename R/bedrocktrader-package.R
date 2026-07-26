fed5a2aec3e2432e3006ebfa1c3646d8386e02ed
RentoSaijo <rentosaijo0527@gmail.com>
2026-07-26 10:26:51 -0400

    Set package skeleton

---
+++ R/bedrocktrader-package.R
@@ -0,0 +1,11 @@
+# Package Documentation ---------------------------------------------------------
+
+#' bedrocktrader: Retrieve Minecraft Bedrock Villager Trade Data
+#'
+#' `bedrocktrader` retrieves official vanilla Minecraft Bedrock villager trade
+#' data from stable Mojang releases and records exact source provenance.
+#'
+#' @name bedrocktrader
+#' @aliases bedrocktrader-package
+
+"_PACKAGE"
